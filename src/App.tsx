98d1682b8098d4fdde529ad27f767973d0e70319
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-14 21:04:47 +0300

    npm install axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
-export default function App() {}
+import OrderForm from "./OrderForm";
+
+export default function App() {
+  const handleOrder = (data: string) => {
+    console.log("Order received from:", data);
+  };
+  return (
+    <>
+      <h1>Place your order</h1>;
+      <OrderForm onSubmit={handleOrder} />;
+    </>
+  );
+}
--- src/OrderForm.tsx
+++ src/OrderForm.tsx
@@ -1,15 +1,16 @@
 interface OrderFormProps {
   onSubmit: (value: string) => void;
 }
-export default function OrderForm(onSubmit: OrderFormProps) {
+export default function OrderForm({ onSubmit }: OrderFormProps) {
   const handleSubmit = (formData: FormData) => {
     const username = formData.get("username") as string;
-    console.log("Name:", username);
-    return (
-      <form action={handleSubmit}>
-        <input type="text" name="username" defaultValue="John Doe" />
-        <button type="submit">Submit</button>
-      </form>
-    );
+    onSubmit(username);
   };
+
+  return (
+    <form action={handleSubmit}>
+      <input type="text" name="username" defaultValue="John Doe" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
